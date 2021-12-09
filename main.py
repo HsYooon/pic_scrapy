0fdc80ba13e83744462b3037254b16d37a6a632a
yhsg2e <yhsg2e@gmail.com>
2021-12-09 17:54:05 +0900

    :zap: json 형식으로 데이터 전송

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from urllib import response
 from flask import Flask
 import datetime
 from bs4 import BeautifulSoup
-import urllib.request
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
-import logging
-
 import requests
 import json
 
@@ -14,29 +11,38 @@ from pic_scrapy.pic_scrapy.spiders.myspider import MyspiderSpider
 
 app = Flask(__name__)
 
-url = 'https://unsplash.com/s/photos/summer'
-
 @app.route('/')
 def home():
-    logging.info('------home()----->')
-    result = fetch()
-    logging.info(f'------result: {result}')
     return "home"
 
+@app.route('/test')
+def test():
+    print("----------test()시작--- ")
+    items = fetch()
+    print("----------fetch()완료--- ")
+    return json.dumps(items)
+
+@app.route('/fetch')
 def fetch():
-    logging.info('------fetch()----->')
-    params = {
-        'spider_name' : 'myspider',
-        'start_requests' : True,
-        'url' : url
-    }
-    response = requests.get('http://localhost:9080/crawl.json', params)
+    print('------fetch()시작----->')
+    # params = {
+    #     'spider_name' : 'myspider',
+    #     'url' : url
+    # }
+    #response = requests.get('http://localhost:9080/crawl.json', params)
+    response = requests.get('http://localhost:9080/crawl.json?spider_name=myspider&callback=parse&url=https://unsplash.com/s/photos/yellow')
     data = json.loads(response.text)
     data_list = []
+    dict = {}
+    id = 0
     for item in data["items"]:
-        data_list.append(item["img"].strip())
+        dict['src'] = item["img"].strip().split(' ')[0]
+        dict['id'] = id
+        dict['sort'] = 'yellow'
+        data_list.append(dict)
+        id += 1
     print(data_list)
-    return data_list
+    return json.dumps(data_list)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='192.168.0.19', debug=True)
--- pic_scrapy/pic_scrapy/spiders/myspider.py
+++ pic_scrapy/pic_scrapy/spiders/myspider.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 class MyspiderSpider(scrapy.Spider):
     name = 'myspider'
-    start_urls = ['https://unsplash.com/s/photos/winter']
+    #start_urls = ['https://unsplash.com/s/photos/winter']
 
     # def __init__(self, category=None, *args, **kwargs):
     #     super(MyspiderSpider,self).__init__(*args, **kwargs)
@@ -14,5 +14,11 @@ class MyspiderSpider(scrapy.Spider):
         for src in response.css('img.YVj9w::attr(srcset)'):
             temp = src.get().split(',')
             yield {
-               'img' : temp[9]
+               'img' : temp[3]
             }
+
+    def start_requests(self, request):
+        url = request.url
+        print("accept_request.rul ---> ")
+        print(url)
+        yield scrapy.Request(url=url, callback=self.parse)
